6ac2a988e031f59f447e4c527f499c9217a5985e
Hermes Flores <hermesrt@gmail.com>
2021-04-08 22:38:24 -0300

    Agregar archivos de proyecto.

---
+++ TodoApp/Models/Task.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoWebApi.Models
+{
+    public class Task
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long TaskGroupId { get; set; }
+        public TaskGroup TaskGroup { get; set; }
+    }
+}
---
+++ TodoApp/Models/TaskGroup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoWebApi.Models
+{
+    public class TaskGroup
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Names { get; set; }
+        public ICollection<Task> Tasks { get; set; }
+
+        public TaskGroup()
+        {
+            Tasks = new HashSet<Task>();
+        }
+    }
+}
