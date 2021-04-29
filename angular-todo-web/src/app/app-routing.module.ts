ca58f1c2e6d2383b981794f5c898b84a216155ea
Hermes Flores <hermesrt@gmail.com>
2021-04-29 12:44:34 -0300

    *Reestructurado el scaffolding de components

--- angular-todo-web/src/app/app-routing.module.ts
+++ angular-todo-web/src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationGuard } from './guards/authentication.guard';
 import { LoggedGuard } from './guards/logged.guard';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { TodoComponent } from './todo/todo.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { TodoComponent } from './components/todo/todo.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "/home", pathMatch: "full" },
