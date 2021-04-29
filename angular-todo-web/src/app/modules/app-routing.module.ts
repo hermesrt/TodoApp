84f3fe754db62154ff5d9e3b47e2fb6c78cdd41e
Hermes Flores <hermesrt@gmail.com>
2021-04-29 12:52:11 -0300

    *modificado el scaffolding de modulos

--- angular-todo-web/src/app/modules/app-routing.module.ts
+++ angular-todo-web/src/app/modules/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthenticationGuard } from './guards/authentication.guard';
-import { LoggedGuard } from './guards/logged.guard';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { TodoComponent } from './components/todo/todo.component';
+import { AuthenticationGuard } from '../guards/authentication.guard';
+import { LoggedGuard } from '../guards/logged.guard';
+import { HomeComponent } from '../components/home/home.component';
+import { LoginComponent } from '../components/login/login.component';
+import { TodoComponent } from '../components/todo/todo.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "/home", pathMatch: "full" },
--- angular-todo-web/src/app/modules/app.module.ts
+++ angular-todo-web/src/app/modules/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/app.component';
+import { AppComponent } from '../components/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ToolbarComponent } from '../components/toolbar/toolbar.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
@@ -19,14 +19,14 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 // Custom component/services imports
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { AuthService } from './services/auth.service';
+import { HomeComponent } from '../components/home/home.component';
+import { LoginComponent } from '../components/login/login.component';
+import { AuthService } from '../services/auth.service';
 
 // Interceptors
-import { AuthInterceptor } from './interceptors/auth.interceptor';
-import { TodoComponent } from './components/todo/todo.component';
-import { SpinnerInterceptor } from './interceptors/spinner.interceptor';
+import { AuthInterceptor } from '../interceptors/auth.interceptor';
+import { TodoComponent } from '../components/todo/todo.component';
+import { SpinnerInterceptor } from '../interceptors/spinner.interceptor';
 
 
 @NgModule({
